8171defe3acf69f8bd274143e876c616e6d42d6e
LvCZ <1092706357@qq.com>
2020-12-01 11:13:51 +0000

    [grrs] 使用 BufReader 优化文件读取

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,5 +1,5 @@
-use std::fs::read_to_string;
 use std::fs::File;
+use std::io::prelude::*;
 use std::io::{BufReader, Result};
 use std::path::PathBuf;
 
@@ -17,7 +17,13 @@ fn main() -> Result<()> {
     let opts = Cli::from_args();
 
     let f = File::open(&opts.path)?;
-    let mut reader = BufReader::new(f);
+    let f = BufReader::new(f);
+
+    for line in f.lines().filter_map(|l| l.ok()) {
+        if line.contains(&opts.pattern) {
+            println!("{}", line);
+        }
+    }
 
     Ok(())
 }
