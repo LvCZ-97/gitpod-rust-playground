6f685c5d8e4933f4c7fa84856fd07d5e67c97dea
LvCZ <1092706357@qq.com>
2020-12-02 04:29:33 +0000

    [grrs] 使用 anyhow 库优化错误提示

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,8 +1,9 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::{BufReader, Result};
+use std::io::BufReader;
 use std::path::PathBuf;
 
+use anyhow::{Context, Result};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -16,7 +17,7 @@ struct Cli {
 fn main() -> Result<()> {
     let opts = Cli::from_args();
 
-    let f = File::open(&opts.path)?;
+    let f = File::open(&opts.path).with_context(|| format!("could not read file `{:?}`", opts.path))?;
     let f = BufReader::new(f);
 
     for line in f.lines().filter_map(|l| l.ok()) {
